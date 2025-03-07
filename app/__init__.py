831165341b28a97c290cff4339feb0652505d2aa
Chella23 <chellaamap@gmail.com>
2025-03-07 11:38:11 +0530

    Add .gitignore to ignore .env file

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from datetime import timedelta
 from flask_session import Session
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 db = SQLAlchemy()
 mail = Mail()
@@ -30,7 +35,7 @@ def create_app():
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USE_SSL'] = False
     app.config['MAIL_USERNAME'] = 'chellaamap@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'ybsw tumb ffta lvqk'
+    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
     app.config['MAIL_DEFAULT_SENDER'] = 'chellaamap@gmail.com'
 
     db.init_app(app)
